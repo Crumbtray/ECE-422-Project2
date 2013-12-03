9ffb1359feda1f43cff0e28f17a4971a02d2a087
Clinton <ciwong@e5-05-16.ece.ualberta.ca>
2013-12-03 13:06:29 -0700

    Server works 2 ways now

--- EncryptionServer/Client.java
+++ EncryptionServer/Client.java
@@ -4,25 +4,37 @@ import java.net.*;
 public class Client {
 	public static void main(String[] args)
 	{
-		if(args.length != 1)
+		if(args.length != 2)
 		{
-			System.err.println("Usage: java Client <host name>");
+			System.err.println("Usage: java Client <host name> <userName>");
 			System.exit(1);
 		}
 		String hostName = args[0];
+		String userName = args[1];
 		int portNumber = 16000;
 		
 		try {
 			Socket socket = new Socket(hostName, portNumber);
+			PrintWriter out = new PrintWriter(socket.getOutputStream());
 			InputStream inputStream = socket.getInputStream();
 			ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-			TestObject to = (TestObject) objectInputStream.readObject();
-			if (to != null)
+			
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			TestObject fromServer;
+			String fromUser;
+			
+			while((fromUser = stdIn.readLine()) != null)
 			{
-				System.out.println(to.getMessage());
+				System.out.println("Client: " + fromUser);
+				out.println(fromUser);
+				out.flush();
+				
+				fromServer = (TestObject) objectInputStream.readObject();
+				if(fromServer != null)
+				{
+					System.out.println("Received Object: " + fromServer.getMessage());
+				}
 			}
-			inputStream.close();
-			socket.close();
 		}
 		catch (Exception e)
 		{
--- EncryptionServer/ServerThread.java
+++ EncryptionServer/ServerThread.java
@@ -1,23 +1,48 @@
 import java.net.*;
+import java.util.*;
 import java.io.*;
 
 
 public class ServerThread extends Thread {
 	private Socket socket = null;
+	private HashMap Users = null;
 	
-	public ServerThread(Socket socket)
+	public ServerThread(Socket socket, HashMap users)
 	{
 		super("ServerThread");
 		this.socket = socket;
+		this.Users = users;
+		System.out.println("Thread created.");
 	}
 	
 	public void run() {
 		try {
+			System.out.println("Starting thread....");
+			InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+			BufferedReader in = new BufferedReader(inputStreamReader);
 			OutputStream outputStream = socket.getOutputStream();
 			ObjectOutputStream oos = new ObjectOutputStream(outputStream);
-			TestObject to = new TestObject("You're a fag");
-			oos.writeObject(to);
-			oos.close();
+			
+			System.out.println("Done initializing streams.");
+			
+			String inputLine;
+			TestObject outputObject;
+			FileServer fileServer = new FileServer();
+			System.out.println("Done initializing FileServer.");
+			
+			while((inputLine = in.readLine()) != null)
+			{
+				System.out.println("Processing input...");
+				if(inputLine.equals("Finish"))
+				{
+					break;
+				}
+				
+				outputObject = fileServer.processInput(inputLine);
+				oos.writeObject(outputObject);
+			}
+						
+			oos.close();			
 			socket.close();
 		}
 		catch (Exception e)
